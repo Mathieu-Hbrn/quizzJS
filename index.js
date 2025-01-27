91b9c6283af22ada119aa465c61d17c4a277808a
mathieu.houbron <mathieu.houbron@campus-centre.fr>
2025-01-27 13:45:41 +0100

    version base 0.3

--- index.js
+++ index.js
@@ -21,97 +21,106 @@ const btn18 = document.getElementById("btn-18")
 const btn19 = document.getElementById("btn-19")
 const btn20 = document.getElementById("btn-20")
 
-const response = document.querySelector("p");
+const response1 = document.querySelector(".box1 > p");
+const response2 = document.querySelector(".box2 > p");
+const response3 = document.querySelector(".box3 > p");
+const response4 = document.querySelector(".box4 > p");
+const response5 = document.querySelector(".box5 > p");
+const response6 = document.querySelector(".box6 > p");
+const response7 = document.querySelector(".box7 > p");
+const response8 = document.querySelector(".box8 > p");
+const response9 = document.querySelector(".box9 > p");
+const response10 = document.querySelector(".box10 > p");
 
 btn1.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response1.classList.add("show-response");
+    response1.style.background = "red";
 })
 
 btn2.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response1.classList.add("show-response");
+    response1.style.background = "green";
 })
 
 btn3.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response2.classList.add("show-response");
+    response2.style.background = "green";
 })
 
 btn4.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response2.classList.add("show-response");
+    response2.style.background = "red";
 })
 
 btn5.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response3.classList.add("show-response");
+    response3.style.background = "red";
 })
 
 btn6.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response3.classList.add("show-response");
+    response3.style.background = "green";
 })
 btn7.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response4.classList.add("show-response");
+    response4.style.background = "green";
 })
 
 btn8.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response4.classList.add("show-response");
+    response4.style.background = "red";
 })
 btn9.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response5.classList.add("show-response");
+    response5.style.background = "red";
 })
 
 btn10.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response5.classList.add("show-response");
+    response5.style.background = "green";
 })
 btn11.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response6.classList.add("show-response");
+    response6.style.background = "red";
 })
 
 btn12.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response6.classList.add("show-response");
+    response6.style.background = "green";
 })
 btn13.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response7.classList.add("show-response");
+    response7.style.background = "green";
 })
 
 btn14.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response7.classList.add("show-response");
+    response7.style.background = "red";
 })
 btn15.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response8.classList.add("show-response");
+    response8.style.background = "green";
 })
 
 btn16.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response8.classList.add("show-response");
+    response8.style.background = "red";
 })
 btn17.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response9.classList.add("show-response");
+    response9.style.background = "red";
 })
 
 btn18.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response9.classList.add("show-response");
+    response9.style.background = "green";
 })
 btn19.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "red";
+    response10.classList.add("show-response");
+    response10.style.background = "red";
 })
 
 btn20.addEventListener("click", () =>{
-    response.classList.add("show-response");
-    response.style.background = "green";
+    response10.classList.add("show-response");
+    response10.style.background = "green";
 })
